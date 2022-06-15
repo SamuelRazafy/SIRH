f0c02506252c6be72e98547a9fef8238a2e2a822
Sam <salamaikoto@gmail.com>
2022-06-15 10:30:01 +0300

    andramako ihany

--- Interface_bienvenue/GestionPersonnelle.xaml.cs
+++ Interface_bienvenue/GestionPersonnelle.xaml.cs
@@ -23,5 +23,10 @@ namespace Interface_bienvenue
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
